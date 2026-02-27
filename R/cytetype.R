8ccf3ac0747e551c54e1356bade4334fa605e917
Yi Su <90744702+suu-yi@users.noreply.github.com>
2026-02-27 15:34:18 +0100

    Revert "Update cytetype.R"
    This reverts commit 8add6f3c0e902e385a37ac2bdf3df655e77fa18c.

--- R/cytetype.R
+++ R/cytetype.R
@@ -89,7 +89,7 @@ PrepareCyteTypeR <- function(obj,
     # Map cluster ids to use those natural numbers
     names(group_metadata) <- cluster_map[names(group_metadata)]
 
-  } else{group_metadata <- setNames(list(), character(0))
+  } else{group_metadata <- list()
   }
   print(paste("Preparing marker genes with top",n_top_genes,"genes..."))
 
